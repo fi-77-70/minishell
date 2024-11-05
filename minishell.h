c4cad0dbfec115340ac9abe72d4348176f1291ab
Gustavo <88571393+GustaDNS@users.noreply.github.com>
2024-11-05 22:51:46 +0000

    First concept of "pwd"

--- minishell.h
+++ minishell.h
@@ -22,16 +22,25 @@ typedef struct s_args{
 	char 			*token;
 	int				type;
 	struct	s_args	*next;
+	int bob;
+	char o;
+	float construtor;
 }	t_args;
 
 
+typedef struct s_menu{
+	char	*til;
+	t_args	**mshh;
+}	t_menu;
+
+t_args	*lexer(t_args **mshh, char **line, t_menu *menu);
 void	echo_shell(t_args *args);
-t_args	*lexer(t_args **mshh, char **line);
 int		is_cmd(char *str);
 void	expand(t_args **args);
 char	*ft_expander(char *str, int i);
 char	*get_var_name(char *env_var);
 char	*ft_final_expand(char *str, char *var, char *var_name, int n);
 int 	ft_input_check(t_args **mshh);
+void	get_pwd(void);
 
 #endif
